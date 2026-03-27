79bc45f5bbfe43995e456c09b0bf4b9fd4b1d223
Vicenfa07 <vicenmilangass@gmail.com>
2026-03-27 11:34:57 -0300

    Añadí objetos básicos

---
+++ src/modelos/Comision.java
@@ -0,0 +1,16 @@
+package modelos;
+
+public class Comision 
+{
+    private Alumno[] alumnos = new Alumno[50];
+    private int id;
+
+    public Alumno[] getAlumnos()
+    {
+        return alumnos;
+    }
+    public void agregarAlumno()
+    {
+
+    }
+}
---
+++ src/modelos/Materia.java
@@ -0,0 +1,30 @@
+package modelos;
+
+public class Materia 
+{
+    private Comision[] comisiones = new Comision[3]; 
+    private String nombre;
+    
+    public void Materia(Comision[] comisiones, String nombre)
+    {
+        this.comisiones = comisiones;
+        this.nombre = nombre;
+    }
+    public void Materia(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public Comision[] getComision()
+    {
+        return comisiones;
+    }
+    public String getNombre()
+    {
+        return nombre;
+    }
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+}
