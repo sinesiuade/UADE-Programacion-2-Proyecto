789399775130eeaccc8dade399995655f767f466
sinesiuade <dsinesi@uade.edu.ar>
2026-04-20 15:45:00 -0300

    Actualicé el Main
    - Actualicé el Main para que soporte errores y permita solucionarlos

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 import modelos.SistemaDeMatricula;
+import modelos.consola.Consola;
 
 public class Main {
     public static void main(String[] args) {
-        SistemaDeMatricula sistema = new SistemaDeMatricula();
+        try {
+            SistemaDeMatricula sistema = new SistemaDeMatricula();
 
-        sistema.ejecutar();
+            sistema.ejecutar();
+        } catch (NullPointerException e) {
+            System.out.println(Consola.ROJO + "Referencia a Null");
+            System.out.println("Detalle técnico: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println(Consola.ROJO + "Ocurrió un error inesperado: " + e.getMessage());
+            e.printStackTrace();
+        }
+        while (true) {
+
+        }
     }
 }
